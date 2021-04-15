916c68ca7a233a2200543ab8cd0662ea259e41b7
Jovana Jovanovic <jjovana314@gmail.com>
2021-04-15 13:40:25 +0200

    post method in Register class update

--- web/app.py
+++ web/app.py
@@ -6,6 +6,7 @@ from flask_jwt_extended import create_access_token, create_refresh_token, JWTMan
 import exceptions
 import login
 import schema_validation
+import register
 
 
 # todo: token should last only 20 minutes
@@ -59,28 +60,45 @@ class Register(Resource):
         global format_issue_date
         posted_data = request.get_json()    # this is list with dictionaries
         id_invoices = []
+        max_invoices_request = 1000
 
         liability = dict()
         liability_error = dict()        # this is null if we got no errors
         liability_error["InvoiceNumber"] = []
+        liability["InvoiceNumber"] = []
+        liability_error["LimitError"] = None
+        liability_error["SchemaError"] = None
 
         result_dict = dict()
+        idf_list = []
+
+        if len(posted_data) > max_invoices_request:
+            liability_error["LimitError"] = f"Invoice limit per request is {max_invoices_request}"
 
         for curr_invoice in posted_data:
             try:
                 schema_validation.schema_generator(curr_invoice, "schema_register")
             except exceptions.SchemaError as ex:
-                liability_error["SchemaError"] = ex[0]
+                liability_error["SchemaError"] = ex.args[0]
 
             issue_date = curr_invoice["IssueDate"]
             invoice_number = curr_invoice["InvoiceNumber"]
+            idf_list.append(invoice_number)
 
             try:
-                date_formated = validate_date_time(format_issue_date, issue_date)
-            except ValueError as ex:
+                date_formated = register.validate_date_time(format_issue_date, issue_date)
+            except ValueError:
                 liability_error["InvoiceNumber"].append(invoice_number)
             else:
-                id_invoices.append(invoice_number)
+                liability["InvoiceNumber"].append(invoice_number)
+        
+        if len(list(liability_error.values())) == 0:
+            liability_error = None
+
+        result_dict["liability"] = liability
+        result_dict["liabilityError"] = liability_error
+        result_dict["IDFList"] = idf_list
+        return result_dict
 
 
 api.add_resource(Login, "/api/login")
