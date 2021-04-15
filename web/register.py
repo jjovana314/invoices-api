f30af957d4136bf1030446b6d92076e27c0b87b1
Jovana Jovanovic <jjovana314@gmail.com>
2021-04-15 13:42:24 +0200

    bug fixed in function validate_date_time

--- web/register.py
+++ web/register.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 
 def validate_date_time(format_datetime: str, date_time: str) -> datetime:
     try:
-        result = datetime.strptime(format_datetime, date_time)
+        result = datetime.strptime(date_time, format_datetime)
     except ValueError:
         raise ValueError from None
     else:
