01333654afd93aa57f4a17a286aee8c64372b950
Jovana Jovanovic <jjovana314@gmail.com>
2021-04-21 18:16:03 +0200

    bug fixed in functions for schema validation

--- web/schema_validation.py
+++ web/schema_validation.py
@@ -16,23 +16,23 @@ def validate_schema(server_data: dict) -> None:
         SchemaError: if data dictionary is not valid
     """
     # we want json data, so we have to dump our data into json string
-    data = dumps(server_data)
+    # data = dumps(server_data)
     global schema
     try:
         # try to do validation for our json data
-        validate(loads(data), schema)
+        validate(server_data, schema)
     except ValidationError as ex:
         ex_str = str(ex)
         for idx, value in enumerate(exceptions.schema_errors):
             # create appropriate message for user
             # if there is exception occured
             if value in ex_str:
-                raise exceptions.schema_exceptions[idx](exceptions.error_messages[idx])
+                raise exceptions.schema_exceptions[idx](exceptions.error_messages[idx]) from None
 
 
 def schema_generator(data: dict, schema_file_name: str) -> None:
     global schema
     schema_file_ext = schema_file_name + ".json"
-    with open(schema_file_ext) as f:
+    with open(schema_file_ext, "r") as f:
         schema = load(f)
     validate_schema(data)
