6921f59c478745e9022f85c0a9007aee902bc04c
Jovana Jovanovic <jjovana314@gmail.com>
2021-05-07 11:44:23 +0200

    generate_idf function created

--- web/register.py
+++ web/register.py
@@ -9,3 +9,7 @@ def validate_date_time(format_datetime: str, date_time: str) -> datetime:
         raise ValueError from None
     else:
         return result
+
+
+def generate_idf(invoice_number):
+    return invoice_num.split(" ")[1].split("/")[0] + "F"
