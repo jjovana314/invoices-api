3c8aa4dcc302d795a4483aa060a88de565d97aaa
Jovana Jovanovic <jjovana314@gmail.com>
2021-05-25 13:25:55 +0200

    variable names updated

--- web/tests/login_test.py
+++ web/tests/login_test.py
@@ -3,41 +3,30 @@ import requests
 from http import HTTPStatus
 
 
-url = "http://127.0.0.1:5000"
-path = f"{url}/api/login"
+url = "http://127.0.0.1:5000/api/login"
+headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
 
 
 def test_login():
-    data = """
-    {
-        "login": "name",
-        "password": "pass"
-    }
-    """
-    r = requests.post(path, data)
-    assert r.status_code == 200
+    data = {"login": "name", "password": "pass"}
+    r = requests.post(url, json=data, headers=headers)
+    assert r.status_code == HTTPStatus.OK.value
 
 
 def test_fail_schema_name_login():
-    data = """
-    {
-        "password: "pass"
-    }
-    """
+    data = {"password": "pass"}
+
     msg = {"Code": HTTPStatus.BAD_REQUEST, "Message": "Schema is not valid"}
 
-    r = requests.post(path, data)
-    assert r.status_code == HTTPStatus.OK
+    r = requests.post(url, json=data, headers=headers)
+    assert r.status_code == HTTPStatus.OK.value
     assert r.json() == msg
 
 
 def test_fail_schema_password_login():
-    data = """
-    {
-        "username": "name"
-    }
-    """
+    data = {"username": "name"}
+
     msg = {"Code": HTTPStatus.BAD_REQUEST, "Message": "Schema is not valid"}
-    r = requests.post(path, data)
-    assert r.status_code == HTTPStatus.OK
+    r = requests.post(url, json=data, headers=headers)
+    assert r.status_code == HTTPStatus.OK.value
     assert r.json() == msg
