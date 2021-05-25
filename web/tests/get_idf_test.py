5265ddc015203e88735476087af07326a7ca680d
Jovana Jovanovic <jjovana314@gmail.com>
2021-05-25 13:25:30 +0200

    variable names updated

--- web/tests/get_idf_test.py
+++ web/tests/get_idf_test.py
@@ -3,20 +3,19 @@ import requests
 from http import HTTPStatus
 
 
-url = "http://127.0.0.1:5000"
-path = f"{url}/api/invoice/"
+url = "http://127.0.0.1:5000/api/invoice/"
 
 
 def test_idf_ok():
     invoice_id = "1918F"
-    full_get_path = f"{path}/{invoice_id}"
+    full_get_path = f"{url}/{invoice_id}"
     r = requests.get(full_get_path)
     assert r.status_code == HTTPStatus.OK
 
 
 def test_idf_not_exist():
     invoice_id = "1111F"
-    full_get_path = f"{path}/{invoice_id}"
+    full_get_path = f"{url}/{invoice_id}"
     r = requests.get(full_get_path)
     response = {"code": 1, "message": "Unsuccess"}
     assert r.json()["status"] == response
@@ -24,7 +23,7 @@ def test_idf_not_exist():
 
 def test_invalid_idf_format():
     invoice_id = "AA00"
-    full_get_path = f"{path}/{invoice_id}"
+    full_get_path = f"{url}/{invoice_id}"
     r = requests.get(full_get_path)
     response = {"code": 1, "message": "Unsuccess"}
     assert r.json()["status"] == response
@@ -32,7 +31,7 @@ def test_invalid_idf_format():
 
 def test_valid_id_liability():
     idf = "1923F"
-    full_get_path = f"{path}/{idf}"
+    full_get_path = f"{url}/{idf}"
     r = requests.get(full_get_path)
     assert r.json()["liability"]["invoiceId"] == idf
     assert r.json()["status"]["code"] == 0
@@ -42,7 +41,7 @@ def test_multiple_requests():
     first_invoice = 16
     last_invoice = 21
     idf_list = [f"19{num}F" for num in range(first_invoice, last_invoice)]
-    path_list = [f"{path}{idf}" for idf in idf_list]
+    path_list = [f"{url}{idf}" for idf in idf_list]
     for idx_path_invoice in range(len(path_list)-1):
         r = requests.get(path_list[idx_path_invoice])
         assert r.json()["liability"]["invoiceId"] == idf_list[idx_path_invoice]
