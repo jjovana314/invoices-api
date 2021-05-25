3a62c70b8e4d86cc13170163f409344b766cc9b2
Jovana Jovanovic <jjovana314@gmail.com>
2021-05-25 10:49:48 +0200

    functions for testing added

--- web/tests/get_idf_test.py
+++ web/tests/get_idf_test.py
@@ -20,3 +20,31 @@ def test_idf_not_exist():
     r = requests.get(full_get_path)
     response = {"code": 1, "message": "Unsuccess"}
     assert r.json()["status"] == response
+
+
+def test_invalid_idf_format():
+    invoice_id = "AA00"
+    full_get_path = f"{path}/{invoice_id}"
+    r = requests.get(full_get_path)
+    response = {"code": 1, "message": "Unsuccess"}
+    assert r.json()["status"] == response
+
+
+def test_valid_id_liability():
+    idf = "1923F"
+    full_get_path = f"{path}/{idf}"
+    r = requests.get(full_get_path)
+    assert r.json()["liability"]["invoiceId"] == idf
+    assert r.json()["status"]["code"] == 0
+
+
+def test_multiple_requests():
+    first_invoice = 16
+    last_invoice = 21
+    idf_list = [f"19{num}F" for num in range(first_invoice, last_invoice)]
+    path_list = [f"{path}{idf}" for idf in idf_list]
+    for idx_path_invoice in range(len(path_list)-1):
+        r = requests.get(path_list[idx_path_invoice])
+        assert r.json()["liability"]["invoiceId"] == idf_list[idx_path_invoice]
+        assert r.json()["status"]["code"] == 0
+
