b02f78e00d088c286ac5fe6b0ad1e3aeaaa5b6e7
Jovana <jjovana314@gmail.com>
2021-05-27 09:29:22 +0200

    test functions added

--- web/tests/change_amount_test.py
+++ web/tests/change_amount_test.py
@@ -27,5 +27,37 @@ def test_change_amount_ok_multiple():
     start_amount = 5000
     end_amount = 5005
     invoice_ids = [f"20{num}F" for num in range(first_invoice, last_invoice)]
-    # todo: u petlji kreirati zahtev i proslediti podatke
+    comments = [f"Comment {idx}" for idx in range(first_invoice, last_invoice)]
+    status_response = HTTPStatus.OK
+    message = "Amount updated successfully"
+    for idx in range(len(invoice_ids)-1):
+        amount = randint(start_amount, end_amount)
+        data = {"invoiceId": invoice_ids[idx], "amount": amount, "comments": comments[idx]}
+        r = requests.post(url, json=data, headers=headers)
+        assert r.json()["Message"] == message
+        assert r.json()["Code"] == status_response
 
+
+def test_change_amount_invalid_multiple():
+    first_invoice = 11
+    last_invoice = 16
+    start_amount = 5000
+    end_amount = 5005
+    invoice_ids = [f"20{num}!@44F" for num in range(first_invoice, last_invoice)]
+    comments = [f"Comment {idx}" for idx in range(first_invoice, last_invoice)]
+    response = {"Message": "Invoice does not exist.", "Code": HTTPStatus.BAD_REQUEST}
+    for idx in range(len(invoice_ids)-1):
+        amount = randint(start_amount, end_amount)
+        data = {"invoiceId": invoice_ids[idx], "amount": amount, "comments": comments[idx]}
+        r = requests.post(url, json=data, headers=headers)
+        assert r.json() == response
+
+
+def test_change_amount_negative_value():
+    invoice_id = "2013F"
+    amount = -500
+    comments = "Comment 1"
+    data = {"invoiceId": invoice_id, "amount": amount, "comments": comments}
+    status_response = HTTPStatus.OK
+    r = requests.post(url, json=data, headers=headers)
+    assert r.json()["Code"] != status_response
