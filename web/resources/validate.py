58e1e583c0a563b19ba8b2c25d8fa59bb228620d
Jovana Jovanovic <jjovana314@gmail.com>
2021-06-21 14:54:02 +0200

    functions validate_bank_num, update_settlement_error_if_invoice_exist, validate_amount, validate_status added in moduele

--- web/resources/validate.py
+++ web/resources/validate.py
@@ -6,46 +6,71 @@ from config import invoices
 from resources.invoice_status import InvoiceStatus
 
 
+
+settlement_error = dict()
+_bank_num = 840      # this value is constant
+
+
 class Validate(Resource):
     """ Validate settlment invoices. """
     def post(self):
         server_data = request.get_json()
         settlement = dict()
-        settlement_error = dict()
-        _bank_num = 840      # this value is constant
         result = []
+        global settlement_error
+        global _bank_num
         for settled_invoice in server_data:
             message, code = validate_schema_caller(settled_invoice, "schema_validate")
             if code != HTTPStatus.OK:
                 settlement_error = {"Message": message, "Code": code}
             else:
-                if int(settled_invoice["bank"]) != _bank_num:
-                    settlement_error = {
-                        "Message": f"Bank number can be only {_bank_num}",
-                        "Code": HTTPStatus.BAD_REQUEST
-                    }
+                validate_bank_num(settled_invoice)
+                
                 idf = generate_idf(settled_invoice["invoiceNumber"])
-                if not invoice_exist("invoiceId", idf):
-                    settlement_error = {
-                        "Message": f"Invoice with id {idf} does not exist",
-                        "Code": HTTPStatus.BAD_REQUEST
-                    }
+                update_settlement_error_if_invoice_exist(idf)
+                
                 settled_amount = settled_invoice["settledAmount"]
                 curr_idf = generate_idf(settled_invoice["invoiceNumber"])
                 amount = invoices.find_one({"invoiceId": curr_idf})["Amount"]
-                if settled_amount > amount:
-                    settlement_error = {
-                        "Message": "setteled amount cannot be greather than invoice amount",
-                        "Code": HTTPStatus.BAD_REQUEST
-                    }
+                validate_amount(settled_amount, amount)
+                
                 status_curr_idf = invoices.find_one({"invoiceId": curr_idf})["Status"]
-                if is_invalid_status_invoice(status_curr_idf):
-                    settlement_error = {
-                        "Message": "Invoice is invalid or canceled or settled",
-                        "Code": HTTPStatus.BAD_REQUEST
-                    }
+                validate_status(status_curr_idf)
             if len(list(settlement_error.values())) > 0:
                 return jsonify({"settlement": settlement, "settlementError": settlement_error})
             settlement = {"invoiceId": curr_idf}
             result.append({"settlement": settlement, "settlementError": settlement_error})
         return jsonify(result)
+
+
+def validate_bank_num(settled_invoice: dict) -> None:
+    global settlement_error
+    global _bank_num
+    if int(settled_invoice["bank"]) != _bank_num:
+        settlement_error = {"Message": f"Bank number can be only {_bank_num}", "Code": HTTPStatus.BAD_REQUEST}
+
+
+def update_settlement_error_if_invoice_exist(idf: str) -> None:
+    global settlement_error
+    if not invoice_exist("invoiceId", idf):
+        settlement_error = {
+            "Message": f"Invoice with id {idf} does not exist",
+                "Code": HTTPStatus.BAD_REQUEST
+            }
+
+
+def validate_amount(settled_amount: int, amount: int) -> None:
+    global settlement_error
+    if settled_amount > amount:
+        settlement_error = {
+            "Message": "setteled amount cannot be greather than invoice amount",
+            "Code": HTTPStatus.BAD_REQUEST
+        }
+
+
+def validate_status(status_curr_idf: int) -> None:
+    if is_invalid_status_invoice(status_curr_idf):
+        settlement_error = {
+            "Message": "Invoice is invalid or canceled or settled",
+            "Code": HTTPStatus.BAD_REQUEST
+        }
