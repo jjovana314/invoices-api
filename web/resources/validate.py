ab90e1ae32169acd6b46342b7c637660531e330a
Jovana <jjovana314@gmail.com>
2021-06-21 13:29:56 +0200

    settlement_error dictionary updated, added if statement

--- web/resources/validate.py
+++ web/resources/validate.py
@@ -18,35 +18,33 @@ class Validate(Resource):
             message, code = validate_schema_caller(settled_invoice, "schema_validate")
             if code != HTTPStatus.OK:
                 settlement_error = {"Message": message, "Code": code}
-                return jsonify({"settlement": settlement, "settlementError": settlement_error})
-            if int(settled_invoice["bank"]) != _bank_num:
-                settlement_error = {
-                    "Message": f"Bank number can be only {_bank_num}",
-                    "Code": HTTPStatus.BAD_REQUEST
-                }
-                return jsonify({"settlement": settlement, "settlementError": settlement_error})
-            idf = generate_idf(settled_invoice["invoiceNumber"])
-            if not invoice_exist("invoiceId", idf):
-                settlement_error = {
-                    "Message": f"Invoice with id {idf} does not exist",
-                    "Code": HTTPStatus.BAD_REQUEST
-                }
-                return jsonify({"settlement": settlement, "settlementError": settlement_error})
-            settled_amount = settled_invoice["settledAmount"]
-            curr_idf = generate_idf(settled_invoice["invoiceNumber"])
-            amount = invoices.find_one({"invoiceId": curr_idf})["Amount"]
-            if settled_amount > amount:
-                settlement_error = {
-                    "Message": "setteled amount cannot be greather than invoice amount",
-                    "Code": HTTPStatus.BAD_REQUEST
-                }
-                return jsonify({"settlement": settlement, "settlementError": settlement_error})
-            status_curr_idf = invoices.find_one({"invoiceId": curr_idf})["Status"]
-            if is_invalid_status_invoice(status_curr_idf):
-                settlement_error = {
-                    "Message": "Invoice is invalid or canceled or settled",
-                    "Code": HTTPStatus.BAD_REQUEST
-                }
+            else:
+                if int(settled_invoice["bank"]) != _bank_num:
+                    settlement_error = {
+                        "Message": f"Bank number can be only {_bank_num}",
+                        "Code": HTTPStatus.BAD_REQUEST
+                    }
+                idf = generate_idf(settled_invoice["invoiceNumber"])
+                if not invoice_exist("invoiceId", idf):
+                    settlement_error = {
+                        "Message": f"Invoice with id {idf} does not exist",
+                        "Code": HTTPStatus.BAD_REQUEST
+                    }
+                settled_amount = settled_invoice["settledAmount"]
+                curr_idf = generate_idf(settled_invoice["invoiceNumber"])
+                amount = invoices.find_one({"invoiceId": curr_idf})["Amount"]
+                if settled_amount > amount:
+                    settlement_error = {
+                        "Message": "setteled amount cannot be greather than invoice amount",
+                        "Code": HTTPStatus.BAD_REQUEST
+                    }
+                status_curr_idf = invoices.find_one({"invoiceId": curr_idf})["Status"]
+                if is_invalid_status_invoice(status_curr_idf):
+                    settlement_error = {
+                        "Message": "Invoice is invalid or canceled or settled",
+                        "Code": HTTPStatus.BAD_REQUEST
+                    }
+            if len(list(settlement_error.values())) > 0:
                 return jsonify({"settlement": settlement, "settlementError": settlement_error})
             settlement = {"invoiceId": curr_idf}
             result.append({"settlement": settlement, "settlementError": settlement_error})
