d4a63d851af1725099db5a75623507d31b922d28
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-25 11:28:17 +0200

    documentation updated

--- web/helper.py
+++ web/helper.py
@@ -14,12 +14,12 @@ schema = dict()
 def validate_schema(server_data: dict) -> None:
     """ JSON schema validation.
 
-  Arguments:
-    server_data {dict} -- data from server
+    Arguments:
+        server_data {dict} -- data from server
 
-  Raises:
-    SchemaError: if data dictionary is not valid
-  """
+    Raises:
+        SchemaError: if data dictionary is not valid
+    """
     # we want json data, so we have to dump our data into json string
     # data = dumps(server_data)
     global schema
@@ -41,10 +41,10 @@ def validate_schema(server_data: dict) -> None:
 def schema_generator(data: dict, schema_file_name: str) -> None:
     """ Generate json schema for current data.
 
-  Arguments:
-    data {dict} -- data from user
-    schema_file_name {str} -- file where schema is located
-  """
+    Arguments:
+        data {dict} -- data from user
+        schema_file_name {str} -- file where schema is located
+    """
     global schema
     schema_file_ext = f"schemas/{schema_file_name}.json"
     with open(schema_file_ext, "r") as f:
@@ -55,12 +55,12 @@ def schema_generator(data: dict, schema_file_name: str) -> None:
 def login_validation(server_data: dict) -> None:
     """ Validate login information for current user.
 
-  Arguments:
-    server_data {dict} -- data from user
+    Arguments:
+        server_data {dict} -- data from user
 
-  Raises:
-    LoginException: if login data is not valid
-  """
+    Raises:
+        LoginException: if login data is not valid
+    """
     if server_data.get("login") is None or server_data.get("password") is None:
         raise exceptions.LoginException
 
@@ -77,14 +77,14 @@ def generate_date_time() -> str:
 def login_exception_handler(server_data: dict, counter: int) -> tuple:
     """ Handle LoginException from login_validation function.
 
-  Arguments:
-    server_data {dict} -- data from user
-    counter {int} -- count how many times LoginException was raised
+    Arguments:
+        server_data {dict} -- data from user
+        counter {int} -- count how many times LoginException was raised
 
-  Returns:
-    Tuple with bool and counter value
-    (False if exception occurred, True otherwise)
-  """
+    Returns:
+        Tuple with bool and counter value
+        (False if exception occurred, True otherwise)
+    """
     try:
         login_validation(server_data)
     except exceptions.LoginException:
@@ -97,9 +97,9 @@ def login_exception_handler(server_data: dict, counter: int) -> tuple:
 def validate_date_caller(data: dict) -> None:
     """ Date validation and liability dictionaries update.
 
-  Arguments:
-      data {dict} -- server data
-  """
+    Arguments:
+        data {dict} -- server data
+    """
     issue_date = data["IssueDate"]
     invoice_number = data["InvoiceNumber"]
     global format_issue_date
@@ -113,13 +113,13 @@ def validate_date_caller(data: dict) -> None:
 def validate_schema_caller(data_validation: dict, schema_name: str) -> tuple:
     """ Call schema_generator function.
 
-  Arguments:
-    data_validation {dict} -- data for validation
-    schema_name {str} -- name of file where schema is located (without extension)
+    Arguments:
+        data_validation {dict} -- data for validation
+        schema_name {str} -- name of file where schema is located (without extension)
 
-  Returns:
-    tuple with message and code
-  """
+    Returns:
+        tuple with message and code
+    """
     try:
         schema_generator(data_validation, schema_name)
     except exceptions.SchemaError as ex:
@@ -138,15 +138,15 @@ def invalid_login_note() -> None:
 
 
 def validate_date_time(format_datetime: str, date_time: str) -> datetime:
-    """ Date and time validation,
+    """ Date and time validation
 
-  Arguments:
-    format_datetime {str} -- pattern for formatting
-    date_time {str} -- date and time from user
+    Arguments:
+        format_datetime {str} -- pattern for formatting
+        date_time {str} -- date and time from user
 
-  Returns:
-    formatted datetime object
-  """
+    Returns:
+        formatted datetime object
+    """
     try:
         result = datetime.strptime(date_time, format_datetime)
     except ValueError:
@@ -158,12 +158,12 @@ def validate_date_time(format_datetime: str, date_time: str) -> datetime:
 def generate_idf(invoice_number: str) -> str:
     """ Creating invoice's IDF (id fakture - invoice's id)
 
-  Arguments:
-    invoice_number {str} -- invoice number from user
+    Arguments:
+        invoice_number {str} -- invoice number from user
 
-  Returns:
-    generated invoice's id
-  """
+    Returns:
+        generated invoice's id
+    """
     invoice_num_split = invoice_number.split(" ")[1].split("/")
     return invoice_num_split[0] + invoice_num_split[1] + "F"
 
@@ -171,12 +171,12 @@ def generate_idf(invoice_number: str) -> str:
 def is_invalid_status_invoice(status_curr_idf: int):
     """ Invoice status checking.
 
-  Arguments:
-    status_curr_idf {int} -- status of current invoice
+    Arguments:
+        status_curr_idf {int} -- status of current invoice
 
-  Returns:
-    True if invoice is not valid, False if it is valid
-  """
+    Returns:
+        True if invoice is not valid, False if it is valid
+    """
     return (status_curr_idf == InvoiceStatus.Canceled.code or
             status_curr_idf == InvoiceStatus.Settled.code or
             status_curr_idf == InvoiceStatus.Invalid.code)
